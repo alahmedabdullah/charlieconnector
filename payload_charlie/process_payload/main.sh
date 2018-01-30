2efd3562ab6fb6f8ec414b03b06df435bee7c816
alahmedabdullah <abdullah.cs@gmail.com>
2018-01-30 06:40:54 +0000

    made necessary changes

--- payload_charlie/process_payload/main.sh
+++ payload_charlie/process_payload/main.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 INPUT_DIR=$1
-cp run.sh_template $INPUT_DIR/run.sh_template
 
-cd $INPUT_DIR
-sh ./run.sh $@
+cp run.sh $INPUT_DIR/run.sh
+
+RUN_DIR=`cd "$(dirname "$0")" && pwd`
+
+echo $RUN_DIR > mainsh.output
+
+sh $RUN_DIR/run.sh $@ 
 
 # --- EOF ---
---
+++ payload_charlie/process_payload/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+
+java_exe=$(whereis java 2>&1 | awk '/java/ {print $2}')
+java_path=$(dirname $java_exe)
+
+export PATH=$java_path:$PATH
+
+
+
+cd $INPUT_DIR
+
+#echo $java_exe >> runlog2.txt
+#echo $java_path >> runlog2.txt
+#echo "/opt/charlie/tCharlie.sh $(cat cli_parameters.txt)" >> runlog2.txt
+
+/opt/charlie/tCharlie.sh $(cat cli_parameters.txt) &> runlog.txt
+
+
+cp ./*.txt ../$OUTPUT_DIR
+
+# --- EOF ---
